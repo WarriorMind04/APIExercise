03f63e00d80b6303092b9612fecf64474e9dce59
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2025-03-03 12:31:27 -0600

    Creación del hook

---
+++ src/app/hooks/useRandomPerson.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+interface Person {
+  name: {
+    first: string;
+    last: string;
+  };
+  picture: {
+    large: string;
+  };
+}
+
+const useRandomPerson = () => {
+  const [person, setPerson] = useState<Person | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchNewPerson = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://randomuser.me/api/");
+      const data = await response.json();
+      setPerson(data.results[0]);
+    } catch (error) {
+      console.error("Error fetching new person:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchNewPerson();
+  }, []);
+
+  return {
+    person,
+    loading,
+    fetchNewPerson,
+  };
+};
